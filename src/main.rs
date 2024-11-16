1c9f210606c5307b7081c2a1f89723e86b9618fe
SafarSoFar <so_far_away0525@proton.me>
2024-11-16 11:06:36 +0500

    raw terminal mode, refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,42 +7,61 @@ use serde_json::json;
 use std::{
     env,
     fs::{self, File},
-    io::{self, BufRead, BufReader, Read, Write},
+    io::{self, BufRead, BufReader, Read, Stdout, Write},
     path::Path,
 };
 use termion::{
     color::{self, Reset},
-    cursor::{self, DetectCursorPos},
-    input::TermRead,
-    raw::IntoRawMode,
-    style,
+    cursor::DetectCursorPos,
+    raw::{IntoRawMode, RawTerminal},
 };
+use tokio::io::stdout;
 
 mod resp_structs;
 use resp_structs::*;
 
-fn custom_print_line(string_to_print: String) {
-    print_logo();
-    println!("{}", string_to_print);
-    return;
-}
+static mut TERM_SIZE_X: u16 = 32;
 
-fn print_logo() {
+fn trim_to_fit_term(string_to_trim: String, raw_term: &mut RawTerminal<Stdout>) -> String {
     unsafe {
-        if LOGO_INDEX >= GH_LOGO_VEC.len() {
-            return;
-        }
-        print!(
-            "{}{}{}      ",
-            color::Fg(color::White),
-            GH_LOGO_VEC[LOGO_INDEX],
-            color::Fg(color::Reset),
-        );
-        LOGO_INDEX += 1;
+        let (cur_pos_x, _) = raw_term.cursor_pos().unwrap_or_default();
+        let space_left = (TERM_SIZE_X - cur_pos_x) as i32;
+        string_to_trim.chars().take(space_left as usize).collect()
     }
 }
 
-async fn get_user_info(login: &str) {
+fn custom_print_line(string_to_print: String, raw_term: &mut RawTerminal<Stdout>) {
+    print_logo(raw_term);
+    let truncated: String = trim_to_fit_term(string_to_print, raw_term);
+    println!("{}\r", truncated);
+}
+
+fn get_terminal_size_x() {
+    unsafe {
+        (TERM_SIZE_X, _) = termion::terminal_size().unwrap();
+    }
+}
+
+const space_offset: &str = "      ";
+fn print_logo(raw_term: &mut RawTerminal<Stdout>) {
+    unsafe {
+        let trimmed_logo_part = trim_to_fit_term(GH_LOGO_VEC[LOGO_INDEX].to_string(), raw_term);
+        print!(
+            "{}{}{}{}",
+            color::Fg(color::White),
+            trimmed_logo_part,
+            color::Fg(color::Reset),
+            space_offset
+        );
+
+        // the last index is just empty space offsets
+        if LOGO_INDEX < GH_LOGO_VEC.len() - 1 {
+            LOGO_INDEX += 1;
+        }
+    }
+}
+
+async fn get_user_info(login: &str, raw_term: &mut RawTerminal<Stdout>) {
     let mut url = String::from("https://api.github.com/users/");
     url += &login;
 
@@ -60,20 +79,23 @@ async fn get_user_info(login: &str) {
 
         println!();
 
-        custom_print_line(format!(
-            "User: {} {} ({}) {} ",
-            color::Fg(color::Red),
-            userData.login,
-            userData.name,
-            color::Fg(color::Reset)
-        ));
+        custom_print_line(
+            format!(
+                "User: {} {} ({}) {} ",
+                color::Fg(color::Red),
+                userData.login,
+                userData.name,
+                color::Fg(color::Reset)
+            ),
+            raw_term,
+        );
         //println!(r#"Bio: '{}'"#, userData.bio);
     }
 }
 
 static mut LOGO_INDEX: usize = 0;
 
-const GH_LOGO_VEC: [&str; 33] = [
+const GH_LOGO_VEC: [&str; 34] = [
     "                          @@@@@@@@@                          ",
     "                   @@@@@@@@@@@@@@@@@@@@@@@                   ",
     "               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               ",
@@ -107,13 +129,21 @@ const GH_LOGO_VEC: [&str; 33] = [
     "             @@@@@@@@@@               @@@@@@@@@@             ",
     "                @@@@@@@               @@@@@@@                ",
     "                    @@                 @@                    ",
+    "                                                             ",
+    // the last index is the space offset (Hacky stuff, yeah)
 ];
 
-fn print_border() {
-    println!("_______________________");
+fn print_border(raw_term: &mut RawTerminal<Stdout>) {
+    print_logo(raw_term);
+
+    let border = "_______________________";
+    let trimmed_border = trim_to_fit_term(border.to_string(), raw_term);
+    println!("{}\r", trimmed_border);
+    //print_logo();
+    //println!();
 }
 
-async fn get_user_work_info(login: &str, token: &str) {
+async fn get_user_work_info(login: &str, token: &str, raw_term: &mut RawTerminal<Stdout>) {
     let url = "https://api.github.com/graphql".to_string();
     let json_body = r#"
         query {
@@ -169,35 +199,46 @@ async fn get_user_work_info(login: &str, token: &str) {
 
     let graph_resp_data: GraphRespData = serde_json::from_str(&graph_data).unwrap();
 
-    custom_print_line(format!(
-        "Total contributions {}",
-        graph_resp_data
-            .data
-            .user
-            .contributions_collection
-            .contribution_calendar
-            .total_contributions
-    ));
-    custom_print_line("Pinned Repos".to_string());
-    print_border();
+    custom_print_line(
+        format!(
+            "Total contributions {}",
+            graph_resp_data
+                .data
+                .user
+                .contributions_collection
+                .contribution_calendar
+                .total_contributions
+        ),
+        raw_term,
+    );
 
     for node in graph_resp_data.data.user.pinned_items.nodes {
-        custom_print_line(format!("Repo: {} ", node.name));
-        custom_print_line(format!("{}", node.description));
-        custom_print_line(format!(
-            r#" {}*{} {}   \|/ {}"#,
-            color::Fg(color::Yellow),
-            color::Fg(color::Reset),
-            node.stargazers.total_count,
-            node.forks.total_count
-        ));
+        custom_print_line(
+            format!(
+                "{}{}:{}",
+                color::Fg(color::Red),
+                node.name,
+                color::Fg(color::Reset)
+            ),
+            raw_term,
+        );
+        custom_print_line(format!("{}", node.description), raw_term);
+        custom_print_line(
+            format!(
+                r#" {}*{} {}   \|/ {}"#,
+                color::Fg(color::Yellow),
+                color::Fg(color::Reset),
+                node.stargazers.total_count,
+                node.forks.total_count
+            ),
+            raw_term,
+        );
 
-        print_logo();
-        print_border();
+        print_border(raw_term);
     }
 
     for i in 0..7 {
-        print_logo();
+        print_logo(raw_term);
         for week in graph_resp_data
             .data
             .user
@@ -210,7 +251,7 @@ async fn get_user_work_info(login: &str, token: &str) {
                 print_activity_square(week.contribution_days[i].contribution_count);
             }
         }
-        println!();
+        println!("\r");
     }
 }
 
@@ -257,18 +298,20 @@ fn print_activity_square(contribution_count: u32) {
 
 #[tokio::main]
 async fn main() {
+    let mut raw_term = std::io::stdout().into_raw_mode().unwrap();
+    get_terminal_size_x();
     let args: Vec<String> = env::args().collect();
 
     if is_config_file_exists() {
         let (login, token) = read_config_file().unwrap();
 
-        get_user_info(&login).await;
-        get_user_work_info(&login, &token).await;
+        get_user_info(&login, &mut raw_term).await;
+        get_user_work_info(&login, &token, &mut raw_term).await;
     } else {
         match args.len() {
             1 => panic!("Error: Enter GitHub username + token"),
             2 => {
-                get_user_info(&args[1]).await;
+                get_user_info(&args[1], &mut raw_term).await;
                 println!("Enter GitHub token for full information")
             }
             3 => {
@@ -285,8 +328,8 @@ async fn main() {
                     create_config_file(&args[1], &args[2])
                 }
 
-                get_user_info(&args[1]).await;
-                get_user_work_info(&args[1], &args[2]).await;
+                get_user_info(&args[1], &mut raw_term).await;
+                get_user_work_info(&args[1], &args[2], &mut raw_term).await;
             }
             _ => {}
         }
@@ -295,8 +338,8 @@ async fn main() {
     // Starting from index 1 because the first argument is the binary location
 
     unsafe {
-        while LOGO_INDEX < GH_LOGO_VEC.len() {
-            print_logo();
+        while LOGO_INDEX < GH_LOGO_VEC.len() - 1 {
+            print_logo(&mut raw_term);
             println!();
         }
     }
